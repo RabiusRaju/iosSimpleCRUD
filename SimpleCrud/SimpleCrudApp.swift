d7cc9c5dce63982819f1865585f6c256e7a91e40
Md.Rabius sani raju <rabiussani8121@gmail.com>
2023-02-20 20:04:09 +0400

    Initial Commit

---
+++ SimpleCrud/SimpleCrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCrudApp.swift
+//  SimpleCrud
+//
+//  Created by Md. Rabius Sani Raju on 20/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
